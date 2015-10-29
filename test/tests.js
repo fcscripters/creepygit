f58300699ab5dcf70b21949e4ed7e4f7131b3ffc
Conor C <Conorc1000@gmail.com>
2015-10-29 11:17:05 +0000

    set up tests

---
+++ test/tests.js
@@ -0,0 +1,14 @@
+var tape = require('tape');
+var shot = require('shot');
+var assert = require('assert');
+var generalHandler = require('../handlers/generalHandler');
+var testrequest = requre('request');
+var router = require('routes')();
+
+tape("check to see if the server is running ok", function (t) {
+  shot.inject(generalHandler, {method: 'get', url: '/'},function (res){
+    var result = res.statusCode;
+    t.equal(result, 200, "success!");
+    t.end();
+  });
+});
